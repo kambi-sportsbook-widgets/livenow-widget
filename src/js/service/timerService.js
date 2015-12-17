b0afb27c756e41edde711cd769d2720aea04cee8
Andrei Teodorescu <andrei@globalmouth.com>
2015-12-17 17:45:20 +0100

    KSBWI-52

---
+++ src/js/service/timerService.js
@@ -0,0 +1,37 @@
+(function () {
+
+   'use strict';
+
+   (function ( $app ) {
+
+      /**
+       * TimerService
+       */
+      return $app.service('timerService', ['$rootScope', '$interval', function ( $rootScope, $interval ) {
+
+         var timerService = {};
+
+         timerService.seconds = 0;
+
+         /**
+          * Start timer and broadcast an event
+          */
+         timerService.start = function() {
+            timerService.interval = $interval(function() {
+               timerService.seconds++;
+               $rootScope.$broadcast('TIMER:UPDATE', timerService.seconds);
+            }, 1000);
+         };
+
+         /**
+          * Stops the timer
+          */
+         timerService.stop = function() {
+            timerService.seconds = 0;
+            $interval.cancel(timerService.interval);
+         };
+
+         return timerService;
+      }]);
+   })(angular.module('livenowWidget'));
+})();
