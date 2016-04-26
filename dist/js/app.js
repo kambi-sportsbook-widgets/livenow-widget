7aac1022e00d81fb858a715930a8fd251115faf9
Andrei Teodorescu <andrei@globalmouth.com>
2016-04-26 15:48:07 +0200

    KSBWI-328 Fix IE11 Flex bug

--- dist/js/app.js
+++ dist/js/app.js
@@ -184,6 +184,44 @@
 
 }).call(this);
 
+(function () {
+
+   'use strict';
+
+   (function ($app) {
+
+      /**
+       * TimerService
+       */
+      return $app.service('timerService', ['$rootScope', '$interval', function ($rootScope, $interval) {
+
+         var timerService = {};
+
+         timerService.seconds = 0;
+
+         /**
+          * Start timer and broadcast an event
+          */
+         timerService.start = function () {
+            timerService.interval = $interval(function () {
+               timerService.seconds++;
+               $rootScope.$broadcast('TIMER:UPDATE', timerService.seconds);
+            }, 1000);
+         };
+
+         /**
+          * Stops the timer
+          */
+         timerService.stop = function () {
+            timerService.seconds = 0;
+            $interval.cancel(timerService.interval);
+         };
+
+         return timerService;
+      }]);
+   })(angular.module('livenowWidget'));
+})();
+
 /**
  * Timer directive
  * @author teo@globalmouth.com
@@ -279,42 +317,4 @@
       }]);
    })(angular.module('livenowWidget'));
 
-})();
-
-(function () {
-
-   'use strict';
-
-   (function ($app) {
-
-      /**
-       * TimerService
-       */
-      return $app.service('timerService', ['$rootScope', '$interval', function ($rootScope, $interval) {
-
-         var timerService = {};
-
-         timerService.seconds = 0;
-
-         /**
-          * Start timer and broadcast an event
-          */
-         timerService.start = function () {
-            timerService.interval = $interval(function () {
-               timerService.seconds++;
-               $rootScope.$broadcast('TIMER:UPDATE', timerService.seconds);
-            }, 1000);
-         };
-
-         /**
-          * Stops the timer
-          */
-         timerService.stop = function () {
-            timerService.seconds = 0;
-            $interval.cancel(timerService.interval);
-         };
-
-         return timerService;
-      }]);
-   })(angular.module('livenowWidget'));
 })();
