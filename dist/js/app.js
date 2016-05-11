48cad253d95101e61f1f0fdc35392f436861d5e2
Daniel Hoffmann <daniel.hoffmann@globalmouth.com>
2016-05-11 15:30:00 +0200

    Updated to new core version

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,320 +1,146 @@
-(function () {
-
-   var arrDependencies;
-
-   arrDependencies = [
-      'widgetCore',
-      'widgetCore.translate',
-      'ngAnimate'
-   ];
-
-   (function ($app) {
-      'use strict';
-      return $app;
-   })(angular.module('livenowWidget', arrDependencies));
-}).call(this);
+'use strict';
 
 (function () {
-
    'use strict';
 
-   function appController ( $scope, $widgetService, $apiService, $controller, timerService, $timeout ) {
-
-      // Extend the core controller that takes care of basic setup and common functions
-      angular.extend(appController, $controller('widgetCoreController', {
-         '$scope': $scope
-      }));
-
-      // Default arguments, these will be overridden by the arguments from the widget api
-      $scope.defaultArgs = {
+   var LiveNowWidget = CoreLibrary.Component.subclass({
+      defaultArgs: {
          listLimit: 1, // Set the list limit value to be used for pagination
-         useFilter: false, // Get the live events from filtering
          fallBackFilter: 'all/all/all/' // Set a fallback filter if we cant get the filter from the url
-      };
+      },
 
-      // Set the initial list limit
-      $scope.initialListLimit = $scope.defaultArgs.listLimit;
+      constructor: function constructor() {
+         CoreLibrary.Component.apply(this, arguments);
+      },
+      init: function init() {
+         // The live events grabbed from the api
+         this.scope.events = [];
 
-      // The live events grabbed from the api
-      $scope.events = [];
+         // timer for updating the running times of the live matches
+         this.timer = null;
 
-      // An array of pages, used for pagination
-      $scope.pages = [];
+         // timer to update the matches themselves
+         this.updateTimer = null;
 
-      // The index to start from
-      $scope.startFrom = 0;
-
-      // Default Widget height, used when resetting the list
-      $scope.defaultHeight = 515;
-
-      // The actual list of the widget
-      // $scope.currentHeight = 515;
-
-      // By default enable animation
-      $scope.enableAnimation = true;
-
-      // Check that the list limit is not set to 0
-      if ( $scope.initialListLimit === 0 ) {
-         $scope.initialListLimit = 3;
-      }
-
-      /**
-       * Fetches the data from the API and sets up pages
-       */
-      $scope.getLiveEvents = function ( params ) {
-         // In case we want to indicate that we are loading data, set the loading flag
-         $scope.loading = true;
-
-         // Use filter to get events or get all live events.
-         // TODO: Remove this implementation once live events can be filtered
-         if ( $scope.args.useFilter ) {
-            $scope.apiService = $apiService.getLiveEventsByFilter;
+         // Set filter parameters
+         var params;
+         if (CoreLibrary.pageInfo.pageType === 'filter') {
+            params = CoreLibrary.pageInfo.pageParam;
          } else {
-            $scope.apiService = $apiService.getLiveEvents;
+            params = this.scope.args.fallBackFilter;
          }
+         // Fetch the live events
+         this.getLiveEvents(params);
+      },
+      startTimer: function startTimer() {
+         this.timerCount = 0;
+         this.updateTimers();
+      },
 
-         return $scope.apiService(params).then(function ( response ) {
-            if ( response.data.events != null ) {
-               $scope.events = response.data.events;
+
+      // frequency of the timer update in milliseconds
+      timerTick: 1000,
+
+      parseTime: function parseTime(secs, mins, running) {
+         var secs = parseInt(secs, 10);
+         var mins = parseInt(mins, 10);
+         if (running) {
+            var elapsedSeconds = this.timerTick * this.timerCount / 1000;
+            var eventSeconds = mins * 60 + secs + elapsedSeconds;
+
+            mins = String(Math.floor(eventSeconds / 60));
+            secs = String(Math.floor(eventSeconds % 60));
+         }
+         if (mins.length === 1) {
+            mins = '0' + mins;
+         }
+         if (secs.length === 1) {
+            secs = '0' + secs;
+         }
+         return mins + ' : ' + secs;
+      },
+      updateTimers: function updateTimers() {
+         var _this = this;
+
+         this.timerCount++;
+         this.scope.events.forEach(function (e) {
+            if (e.liveData == null || e.liveData.matchClock == null) {
+               e.timer = null;
             } else {
-               $scope.events = [];
+               var matchClock = e.liveData.matchClock;
+               e.timer = _this.parseTime(matchClock.second, matchClock.minute, matchClock.running);
             }
+         });
 
-            // Setup the pages
-            $scope.setPages($scope.events, $scope.args.listLimit); // Call the directive function here
+         clearTimeout(this.timer);
+         this.timer = setTimeout(function () {
+            _this.updateTimers();
+         }, this.timerTick);
+      },
+      getLiveEvents: function getLiveEvents(params) {
+         var _this2 = this;
+
+         params = 'football/all/all/';
+         // Use filter to get events or get all live events.
+         return CoreLibrary.offeringModule.getLiveEventsByFilter(params).then(function (response) {
+            clearTimeout(_this2.updateTimer);
+            if (response.events != null) {
+               response.events.forEach(function (e) {
+                  if (e.betOffers != null && e.betOffers.length >= 1) {
+                     e.betOffers = e.betOffers[0];
+                     e.betOffers.outcomesLength = e.betOffers.outcomes.length;
+                  }
+
+                  e.navigateToLiveEvent = function (eventId) {
+                     CoreLibrary.widgetModule.navigateToLiveEvent(eventId);
+                  }.bind(_this2, e.event.id);
+
+                  if (e.liveData && e.liveData.statistics && e.liveData.statistics.setBasedStats && e.liveData.statistics.setBasedStats.home.length) {
+                     e.showScore = true;
+                  } else {
+                     e.showScore = false;
+                  }
+               });
+               _this2.scope.events = response.events;
+               // rivets does not allow you to track an Array length
+               // see https://github.com/mikeric/rivets/issues/278
+               _this2.scope.eventsLength = response.events.length;
+            } else {
+               _this2.scope.events = [];
+               _this2.scope.eventsLength = 0;
+            }
 
             // If there are outcomes in the betslip we need update our event outcomes with this.
             // Request the betslip outcomes.
-            $widgetService.requestBetslipOutcomes();
-
-            // Start the global timer
-            timerService.stop();
-            timerService.start();
-
-            // Enable back the animation after 300ms delay
-            $timeout(function () {
-               $scope.enableAnimation = true;
-            }, 300);
-
-         }, function ( response ) {
-            void 0;
-            void 0;
-         }).finally(function () {
-
-            // Check if the list limit is higher than the actual length of the list, set it to the actual length if so
-            if ( $scope.initialListLimit > $scope.events.length ) {
-               $scope.args.listLimit = $scope.events.length;
-            }
+            CoreLibrary.widgetModule.requestBetslipOutcomes();
 
             // Hide the widget if there are no live events to show
-            if ( $scope.events && $scope.events.length > 0 ) {
-               $widgetService.setWidgetHeight($scope.args.listLimit * 145 + 37 * 2);
+            if (_this2.scope.events && _this2.scope.events.length > 0) {
+               CoreLibrary.widgetModule.setWidgetHeight(_this2.scope.args.listLimit * 145 + 37 * 2);
             } else {
-               $widgetService.setWidgetHeight(0);
+               CoreLibrary.widgetModule.setWidgetHeight(0);
             }
 
-            // Finally we unset the loading flag
-            $scope.loading = false;
+            _this2.pagination = new CoreLibrary.PaginationComponent('#pagination', _this2.scope, 'events', _this2.scope.args.listLimit);
+
+            _this2.startTimer();
+
+            _this2.updateTimer = setTimeout(function () {
+               // TODO prevent animation when updating and keep current page open
+               _this2.getLiveEvents(params);
+            }, 30000);
+         }).catch(function (e) {
+            // the timer resets even if there is an error
+            _this2.updateTimer = setTimeout(function () {
+               _this2.getLiveEvents(params);
+            }, 30000);
+            throw e;
          });
-      };
+      }
+   });
 
-      $scope.toggleOutcome = function ( outcome ) {
-         if ( outcome.selected !== true ) {
-            $scope.addOutcomeToBetslip(outcome);
-         } else {
-            $scope.removeOutcomeFromBetslip(outcome);
-         }
-         // Outcome.selected = !outcome.selected;
-      };
-
-      /**
-       * Iterate over the live events and update the offers with the data from the API
-       * @param {Array} outcomes An array of outcomes that are in the betslip
-       */
-      $scope.updateOutcomes = function ( outcomes ) {
-         var i = 0, eventLen = $scope.events.length;
-         for ( ; i < eventLen; ++i ) {
-            if ( $scope.events[i].betOffers[0] != null && $scope.events[i].betOffers[0].outcomes != null ) {
-               $scope.updateBetOfferOutcomes($scope.events[i].betOffers[0], outcomes);
-            }
-         }
-         $scope.$apply();
-      };
-
-      // Call the init method in the coreWidgetController so that we setup everything using our overridden values
-      // The init-method returns a promise that resolves when all of the configurations are set, for instance the $scope.args variables
-      // so we can call our methods that require parameters from the widget settings after the init method is called
-      $scope.init().then(function () {
-         // Set filter parameters
-         if ( $scope.pageInfo.pageType === 'filter' ) {
-            $scope.params = $scope.pageInfo.pageParam;
-         } else {
-            $scope.params = $scope.args.fallBackFilter;
-         }
-         // Fetch the live events
-         $scope.getLiveEvents($scope.params);
-      });
-
-      // ------- Listeners ----------
-
-      // Betslip outcomes listener
-      $scope.$on('OUTCOMES:UPDATE', function ( event, data ) {
-         $scope.updateOutcomes(data.outcomes);
-      });
-
-      // Odds format listener
-      $scope.$on('ODDS:FORMAT', function ( event, data ) {
-         $scope.setOddsFormat(data);
-      });
-
-      // Listen to timer and refresh events every 30 sec
-      // We disable the animation until after the events are loaded
-      $scope.$on('TIMER:UPDATE', function ( e, count ) {
-         if ( count % 30 === 0 ) {
-            $scope.enableAnimation = false;
-            $scope.getLiveEvents($scope.params);
-         }
-      });
-
-   }
-
-   (function ( $app ) {
-      return $app.controller('appController', ['$scope', 'kambiWidgetService', 'kambiAPIService', '$controller', 'timerService', '$timeout', appController]);
-   })(angular.module('livenowWidget'));
-
-}).call(this);
-
-(function () {
-
-   'use strict';
-
-   (function ($app) {
-
-      /**
-       * TimerService
-       */
-      return $app.service('timerService', ['$rootScope', '$interval', function ($rootScope, $interval) {
-
-         var timerService = {};
-
-         timerService.seconds = 0;
-
-         /**
-          * Start timer and broadcast an event
-          */
-         timerService.start = function () {
-            timerService.interval = $interval(function () {
-               timerService.seconds++;
-               $rootScope.$broadcast('TIMER:UPDATE', timerService.seconds);
-            }, 1000);
-         };
-
-         /**
-          * Stops the timer
-          */
-         timerService.stop = function () {
-            timerService.seconds = 0;
-            $interval.cancel(timerService.interval);
-         };
-
-         return timerService;
-      }]);
-   })(angular.module('livenowWidget'));
+   var liveNowWidget = new LiveNowWidget({
+      rootElement: 'html'
+   });
 })();
-
-/**
- * Timer directive
- * @author teo@globalmouth.com
- */
-(function () {
-
-   'use strict';
-
-   /**
-    * @ngdoc directive
-    * @name livenowWidget.directive:timerDirective
-    * @description
-    * Timer directive displays the event matchClock and calculates the current game time
-    * @restrict E
-    * @scope    *
-    * @author teo@globalmouth.com
-    */
-   (function ( $app ) {
-      return $app.directive('timerDirective', [function () {
-
-         return {
-            restrict: 'E',
-            scope: {
-               'index': '=',
-               'timer': '='
-            },
-            template: '<span ng-if="minute || second" ng-cloak>{{minute}} : {{second}}</span>',
-            controller: ['$scope', '$rootScope', 'timerService', function ( $scope, $rootScope, timerService ) {
-
-               /**
-                * Prepends a zero for numbers below 10
-                * @param time
-                * @returns {*}
-                */
-               var readableTime = function ( time ) {
-                  if ( time < 0 ) {
-                     time = 0;
-                  }
-
-                  if ( time < 10 ) {
-                     time = '0' + time;
-                  }
-                  return time;
-               };
-
-               /**
-                * Transform seconds into minutes and seconds
-                * @param count
-                * @returns {{minutes, seconds}}
-                */
-               var parseSeconds = function ( count ) {
-                  var input_count = parseInt(count, 10);
-                  var minutes = Math.floor(input_count / 60);
-                  var seconds = input_count - (minutes * 60);
-                  return {
-                     minutes: readableTime(minutes),
-                     seconds: readableTime(seconds)
-                  };
-               };
-
-               /**
-                * Get the current event minute and second based on event data and internal timer
-                * @param count
-                */
-               var getRealTime = function ( count ) {
-                  if ( $scope.timer && $scope.timer.running ) {
-                     var eventSeconds = $scope.timer.minute * 60 + $scope.timer.second + count;
-                     $scope.second = parseSeconds(eventSeconds).seconds;
-                     $scope.minute = parseSeconds(eventSeconds).minutes;
-                  }
-               };
-
-               /**
-                * Sets the default value for minute and second based on api data
-                */
-               if ( $scope.timer ) {
-                  if ( $scope.timer.running === false ) {
-                     $scope.minute = readableTime($scope.timer.minute);
-                     $scope.second = readableTime($scope.timer.second);
-                  } else {
-                     getRealTime(timerService.seconds);
-                  }
-               }
-
-               /**
-                * Listener for timer update event, which sets the scope minute and second
-                */
-               $rootScope.$on('TIMER:UPDATE', function ( e, count ) {
-                  getRealTime(count);
-               });
-            }]
-         };
-      }]);
-   })(angular.module('livenowWidget'));
-
-})();
+//# sourceMappingURL=app.js.map
--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,144 @@
 (function () {
+   'use strict';
 
-   var arrDependencies;
+   var LiveNowWidget = CoreLibrary.Component.subclass({
+      defaultArgs: {
+         listLimit: 1, // Set the list limit value to be used for pagination
+         fallBackFilter: 'all/all/all/' // Set a fallback filter if we cant get the filter from the url
+      },
 
-   arrDependencies = [
-      'widgetCore',
-      'widgetCore.translate',
-      'ngAnimate'
-   ];
+      constructor () {
+         CoreLibrary.Component.apply(this, arguments);
+      },
 
-   (function ($app) {
-      'use strict';
-      return $app;
-   })(angular.module('livenowWidget', arrDependencies));
-}).call(this);
+      init () {
+         // The live events grabbed from the api
+         this.scope.events = [];
+
+         // timer for updating the running times of the live matches
+         this.timer = null;
+
+         // timer to update the matches themselves
+         this.updateTimer = null;
+
+         // Set filter parameters
+         var params;
+         if (CoreLibrary.pageInfo.pageType === 'filter') {
+            params = CoreLibrary.pageInfo.pageParam;
+         } else {
+            params = this.scope.args.fallBackFilter;
+         }
+         // Fetch the live events
+         this.getLiveEvents(params);
+      },
+
+      startTimer () {
+         this.timerCount = 0;
+         this.updateTimers();
+      },
+
+      // frequency of the timer update in milliseconds
+      timerTick: 1000,
+
+      parseTime (secs, mins, running) {
+         var secs = parseInt(secs, 10);
+         var mins = parseInt(mins, 10);
+         if (running) {
+            var elapsedSeconds = this.timerTick * this.timerCount / 1000;
+            var eventSeconds = mins * 60 + secs + elapsedSeconds;
+
+            mins = String(Math.floor(eventSeconds / 60));
+            secs = String(Math.floor(eventSeconds % 60));
+         }
+         if (mins.length === 1) {
+            mins = '0' + mins;
+         }
+         if (secs.length === 1) {
+            secs = '0' + secs;
+         }
+         return mins + ' : ' + secs;
+      },
+
+      updateTimers () {
+         this.timerCount++;
+         this.scope.events.forEach((e) => {
+            if (e.liveData == null || e.liveData.matchClock == null) {
+               e.timer = null;
+            } else {
+               var matchClock = e.liveData.matchClock;
+               e.timer = this.parseTime(matchClock.second, matchClock.minute, matchClock.running);
+            }
+         });
+
+         clearTimeout(this.timer);
+         this.timer = setTimeout (() => {
+            this.updateTimers();
+         }, this.timerTick);
+      },
+
+      getLiveEvents (params) {
+         params = 'football/all/all/';
+         // Use filter to get events or get all live events.
+         return CoreLibrary.offeringModule.getLiveEventsByFilter(params).then(( response ) => {
+            clearTimeout(this.updateTimer);
+            if ( response.events != null ) {
+               response.events.forEach((e) => {
+                  if (e.betOffers != null && e.betOffers.length >= 1) {
+                     e.betOffers = e.betOffers[0];
+                     e.betOffers.outcomesLength = e.betOffers.outcomes.length;
+                  }
+
+                  e.navigateToLiveEvent = function (eventId) {
+                     CoreLibrary.widgetModule.navigateToLiveEvent(eventId);
+                  }.bind(this, e.event.id);
+
+                  if (e.liveData && e.liveData.statistics &&
+                        e.liveData.statistics.setBasedStats &&
+                        e.liveData.statistics.setBasedStats.home.length) {
+                     e.showScore = true;
+                  } else {
+                     e.showScore = false;
+                  }
+               });
+               this.scope.events = response.events;
+               // rivets does not allow you to track an Array length
+               // see https://github.com/mikeric/rivets/issues/278
+               this.scope.eventsLength = response.events.length;
+            } else {
+               this.scope.events = [];
+               this.scope.eventsLength = 0;
+            }
+
+            // If there are outcomes in the betslip we need update our event outcomes with this.
+            // Request the betslip outcomes.
+            CoreLibrary.widgetModule.requestBetslipOutcomes();
+
+            // Hide the widget if there are no live events to show
+            if ( this.scope.events && this.scope.events.length > 0 ) {
+               CoreLibrary.widgetModule.setWidgetHeight(this.scope.args.listLimit * 145 + 37 * 2);
+            } else {
+               CoreLibrary.widgetModule.setWidgetHeight(0);
+            }
+
+            this.pagination = new CoreLibrary.PaginationComponent('#pagination', this.scope, 'events', this.scope.args.listLimit);
+
+            this.startTimer();
+
+            this.updateTimer = setTimeout (() => {
+               // TODO prevent animation when updating and keep current page open
+               this.getLiveEvents (params);
+            }, 30000);
+         }).catch((e) => {
+            // the timer resets even if there is an error
+            this.updateTimer = setTimeout (() => {
+               this.getLiveEvents (params);
+            }, 30000);
+            throw e;
+         });
+      }
+   });
+
+   var liveNowWidget = new LiveNowWidget({
+      rootElement: 'html'
+   });
+})();
